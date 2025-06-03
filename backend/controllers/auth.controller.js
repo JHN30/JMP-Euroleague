12a79d75a66ef29f01b96428dfcc166cdd2bbbb3
Jovan Mitic <johanmit03@gmail.com>
2025-06-03 20:24:48 +0200

    Changed routes form GET to POST, added signup functionality and added separated folder for functions and created generateTokenAndSetCookie function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,54 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
 export const signup = async (req, res) => {
-  res.send("signup");
+  const { username, email, password } = req.body;
+
+  try {
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: "Please provide all fields" });
+    }
+
+    const userAlreadyExist = await User.findOne({ email });
+    if (userAlreadyExist) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      verificationToken,
+      verificationTokenExpire: Date.now() + 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+    });
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+				...user._doc,
+				password: undefined,
+			},
+    });
+  } catch (error) {
+    console.log("Error in signup:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
+
 export const login = async (req, res) => {
   res.send("login");
 };
+
 export const logout = async (req, res) => {
   res.send("logout");
 };
