8c1026cc0991a27f83b991b1515ef4bd611efb46
Jovan Mitic <johanmit03@gmail.com>
2025-06-05 12:59:58 +0200

    Updated user model to have roles

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -26,6 +26,11 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
     verificationToken: String,
