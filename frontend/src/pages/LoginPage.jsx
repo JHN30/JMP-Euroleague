ef1b5510eaa239a39dda68c404f098ce87f3bf05
Jovan Mitic <johanmit03@gmail.com>
2025-06-07 18:27:13 +0200

    LoginPage UI finished

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,85 @@
+//eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react";
+import { useState } from "react";
+import { FiLoader } from "react-icons/fi";
+import { FaLock } from "react-icons/fa";
+import { LuMail } from "react-icons/lu";
+import { Link } from "react-router-dom";
+import Input from "../components/Input";
+//import { useAuthStore } from "../store/authStore";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const error = false;
+  const isLoading = false;
+
+  //const { login, isLoading, error } = useAuthStore();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    //await login(email, password);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center relative overflow-hidden">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="max-w-md w-full bg-neutral backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+      >
+        <div className="p-8">
+          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-orange-500 to-orange-400 text-transparent bg-clip-text">
+            Welcome Back
+          </h2>
+
+          <form onSubmit={handleLogin}>
+            <Input
+              icon={LuMail}
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              icon={FaLock}
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div className="flex items-center mb-6">
+              <Link to="/forgot-password" className="text-sm text-orange-400 hover:underline">
+                Forgot password?
+              </Link>
+            </div>
+            {error && <p className="text-red-500 font-semibold mb-2">{error}</p>}
+
+            <motion.button
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+              className="w-full py-3 px-4 bg-gradient-to-r from-orange-500 to-orange-400 text-white font-bold rounded-lg shadow-lg hover:from-orange-600 hover:to-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? <FiLoader className="w-6 h-6 animate-spin mx-auto" /> : "Login"}
+            </motion.button>
+          </form>
+        </div>
+        <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+          <p className="text-sm text-gray-400">
+            Don't have an account?{" "}
+            <Link to="/signup" className="text-orange-400 hover:underline">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+export default LoginPage;
