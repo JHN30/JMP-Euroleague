1a0e56ff519fb1c21819db38fcf6134fc16af05e
Jovan Mitic <johanmit03@gmail.com>
2025-06-29 16:54:20 +0200

    Small security improvment (disableReactDevTools)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
