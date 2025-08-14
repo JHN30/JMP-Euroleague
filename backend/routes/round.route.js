f986fac59f54b62d3141e95be80b6814412ab310
Jovan Mitic <johanmit03@gmail.com>
2025-08-14 21:58:22 +0200

    Protected round routes

--- backend/routes/round.route.js
+++ backend/routes/round.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getRound, createRound, deleteRound, updateRound } from "../controllers/round.controller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", getRound);
-router.post("/", createRound);
-router.delete("/:id", deleteRound);
-router.put("/:id", updateRound);
+router.post("/", verifyToken, createRound);
+router.delete("/:id", verifyToken, deleteRound);
+router.put("/:id", verifyToken, updateRound);
 
 export default router;
