c804b993840370047dd35f543f22f998589f7fd8
Jovan <johanmit03@gmail.com>
2025-10-24 21:57:47 +0200

    Added lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,29 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback, lazy, Suspense } from "react";
 
 import Navbar from "./components/Navbar";
 import { useAuth } from "./hooks/useAuth";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
-import StandingsPage from "./pages/StandingsPage";
-import PredictorPage from "./pages/PredictorPage";
-import PlayoffPage from "./pages/PlayoffPage";
-import TeamsPage from "./pages/TeamsPage";
-import TeamStatsPage from "./pages/TeamStatsPage";
-import ProfilePage from "./pages/ProfilePage";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
-import EmailVerificatonPage from "./pages/EmailVerificationPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
-import AdminPage from "./pages/AdminPage";
+const StandingsPage = lazy(() => import("./pages/StandingsPage"));
+const PredictorPage = lazy(() => import("./pages/PredictorPage"));
+const PlayoffPage = lazy(() => import("./pages/PlayoffPage"));
+const TeamsPage = lazy(() => import("./pages/TeamsPage"));
+const TeamStatsPage = lazy(() => import("./pages/TeamStatsPage"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+const SignUpPage = lazy(() => import("./pages/SignUpPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const EmailVerificatonPage = lazy(() => import("./pages/EmailVerificationPage"));
+const ForgotPasswordPage = lazy(() => import("./pages/ForgotPasswordPage"));
+const ResetPasswordPage = lazy(() => import("./pages/ResetPasswordPage"));
+const AdminPage = lazy(() => import("./pages/AdminPage"));
+
+const FallbackComponent = () => (
+  <div className="flex items-center justify-center h-screen">
+    <LoadingSpinner size="lg" />
+  </div>
+);
 
 function App() {
   const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuth();
@@ -41,37 +47,39 @@ function App() {
   return (
     <div className="flex flex-col mx-auto min-h-screen bg-gradient-to-br from-base-100 via-base-300 to-base-200 relative overflow-hidden">
       {<Navbar didntSignUp={didntSignUp} />}
-      <Routes>
-        {/* Main Pages */}
-        <Route path="/" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <StandingsPage />} />
-        <Route path="/predictor" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <PredictorPage />} />
-        <Route path="/playoff" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <PlayoffPage />} />
-        <Route path="/teams" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <TeamsPage />} />
-        <Route path="/team-stats/:teamId" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <TeamStatsPage />} />
-        <Route path="/profile" element={!isAuthenticated ? <LoginPage /> : <ProfilePage />} />
+      <Suspense fallback={<FallbackComponent />}>
+        <Routes>
+          {/* Main Pages */}
+          <Route path="/" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <StandingsPage />} />
+          <Route path="/predictor" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <PredictorPage />} />
+          <Route path="/playoff" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <PlayoffPage />} />
+          <Route path="/teams" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <TeamsPage />} />
+          <Route path="/team-stats/:teamId" element={!isAuthenticated && !didntSignUp ? <LoginPage /> : <TeamStatsPage />} />
+          <Route path="/profile" element={!isAuthenticated ? <LoginPage /> : <ProfilePage />} />
 
-        {/* Auth */}
-        <Route path="/signup" element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />} />
-        <Route
-          path="/verify-email"
-          element={isAuthenticated && !user.isVerified ? <EmailVerificatonPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/forgot-password"
-          element={isAuthenticated && !didntSignUp ? <Navigate to="/" /> : <ForgotPasswordPage />}
-        />
-        <Route
-          path="/reset-password/:token"
-          element={isAuthenticated && !didntSignUp ? <Navigate to="/" /> : <ResetPasswordPage />}
-        />
+          {/* Auth */}
+          <Route path="/signup" element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />} />
+          <Route path="/login" element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />} />
+          <Route
+            path="/verify-email"
+            element={isAuthenticated && !user.isVerified ? <EmailVerificatonPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/forgot-password"
+            element={isAuthenticated && !didntSignUp ? <Navigate to="/" /> : <ForgotPasswordPage />}
+          />
+          <Route
+            path="/reset-password/:token"
+            element={isAuthenticated && !didntSignUp ? <Navigate to="/" /> : <ResetPasswordPage />}
+          />
 
-        {/* Admin */}
-        <Route
-          path="/admin-dashboard"
-          element={isAuthenticated && !didntSignUp && user?.role === "admin" ? <AdminPage /> : <Navigate to="/" />}
-        />
-      </Routes>
+          {/* Admin */}
+          <Route
+            path="/admin-dashboard"
+            element={isAuthenticated && !didntSignUp && user?.role === "admin" ? <AdminPage /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </Suspense>
       <Toaster />
     </div>
   );
