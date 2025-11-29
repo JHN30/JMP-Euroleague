9fd686a82ff2c3124deda9836e7df740239c8317
Jovan <johanmit03@gmail.com>
2025-11-29 22:29:58 +0100

    Improved PWA functionality

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,7 +1,40 @@
-/* eslint-disable no-undef */
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+const CACHE_NAME = "euroleague-cache-v1";
+const urlsToCache = ["index.html", "offline.html"];
 
-workbox.routing.registerRoute(
-  ({ request }) => request.destination === "image",
-  new workbox.strategies.NetworkFirst()
-);
+const self = this;
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+// Activate the service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
