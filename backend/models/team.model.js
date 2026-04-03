fcb2c8d073e1737c15b51069b72c814cd3688fd5
Jovan <johanmit03@gmail.com>
2026-04-03 20:55:11 +0200

    Rating updates on saving and on updating ratingArray

--- backend/models/team.model.js
+++ backend/models/team.model.js
@@ -88,12 +88,70 @@ const teamsSchema = new mongoose.Schema(
   }
 );
 
-teamsSchema.pre('save', function(next) {
-  // Only update if the array has elements
-  if (this.ratingArray && this.ratingArray.length > 0) {
-    // Take the last element and assign it to the next field
-    this.rating = this.ratingArray[this.ratingArray.length - 1];
+const getLatestRating = (ratingArray) => {
+  return Number(ratingArray[ratingArray.length - 1]);
+};
+
+const syncRatingFromArray = (target) => {
+  const latestRating = getLatestRating(target?.ratingArray);
+  if (latestRating !== null) {
+    target.rating = latestRating;
   }
+};
+
+const syncRatingInUpdate = (update) => {
+  if (!update || Array.isArray(update)) {
+    return;
+  }
+
+  if (Object.prototype.hasOwnProperty.call(update, "ratingArray")) {
+    const latestRating = getLatestRating(update.ratingArray);
+    if (latestRating !== null) {
+      update.rating = latestRating;
+    }
+  }
+
+  if (update.$set && Object.prototype.hasOwnProperty.call(update.$set, "ratingArray")) {
+    const latestRating = getLatestRating(update.$set.ratingArray);
+    if (latestRating !== null) {
+      update.$set.rating = latestRating;
+    }
+  }
+
+  const pushedRating = update.$push?.ratingArray;
+  if (pushedRating !== undefined) {
+    const pushedValues =
+      pushedRating && typeof pushedRating === "object" && Array.isArray(pushedRating.$each)
+        ? pushedRating.$each
+        : [pushedRating];
+
+    const latestRating = getLatestRating(pushedValues);
+    if (latestRating !== null) {
+      update.$set = {
+        ...(update.$set || {}),
+        rating: latestRating,
+      };
+    }
+  }
+};
+
+teamsSchema.pre("save", function (next) {
+  syncRatingFromArray(this);
+  next();
+});
+
+teamsSchema.pre("findOneAndUpdate", function (next) {
+  syncRatingInUpdate(this.getUpdate());
+  next();
+});
+
+teamsSchema.pre("updateOne", function (next) {
+  syncRatingInUpdate(this.getUpdate());
+  next();
+});
+
+teamsSchema.pre("updateMany", function (next) {
+  syncRatingInUpdate(this.getUpdate());
   next();
 });
 
